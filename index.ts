d133a3f76659fe308a7dfad0037c9d5fa58a6fb1
wackyjoker <kevin.young8931@gmail.com>
2021-02-24 06:18:36 +0100

    code runs good on TS environment

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import * as fs from "fs";
 
+(async function(){
+   await console.log("hello");
+    const file_A = "a_example";
+   await pizzaBot(file_A);
+})();
 
-function scanBooks(file) {
+// const file_B = "b_little_bit_of_everything.in";
+// pizzaBot(file_B);
+function pizzaBot(file) {
 
     fs.readFile(file, "ASCII", function(err, data) {
-        let trimStr = data.trim(); // make sure no space or /n at the end or the beginning.
-        let arr = trimStr.split(/\n/g); //split each line into arrays.
+        try {
+            console.log(data);
+            let trimStr = data.trim(); // make sure no space or /n at the end or the beginning.
+            let arr = trimStr.split(/\n/g); //split each line into arrays.
+        } catch (err) {
+            console.error(err);
+        }
+
 
     })
 }
