6cf6b6a195a3f61657d815c0dba7765462c5ca5a
wackyjoker <kevin.young8931@gmail.com>
2021-02-25 19:05:44 +0100

    saving datas

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ let intersections;
 let streets;
 let cars;
 let scores;
+(async function(){
+    const file_A = "in/a.txt";
+    main(file_A);
+})()
+
+
+async function main(file){
+    await fs.readFile(file, "ASCII", function(err, data) {
+        let trimStr = data.trim(); // make sure no space or /n at the end or the beginning.
+        let arr = trimStr.split(/\n/g); //split each line into arrays.
+         console.log(arr);
+    });
+};
+
+console.log("hello")
